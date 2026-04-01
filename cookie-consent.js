8fc189b134d43d738090932599992830710334fb
ghost <m.bateh@proton.me>
2026-04-01 12:09:28 +0200

    fix

--- cookie-consent.js
+++ cookie-consent.js
@@ -20,6 +20,16 @@
     }
   }
 
+  function appendScript(scriptEl) {
+    if (document.head) {
+      document.head.appendChild(scriptEl);
+    } else if (document.body) {
+      document.body.appendChild(scriptEl);
+    } else {
+      document.documentElement.appendChild(scriptEl);
+    }
+  }
+
   function loadNonEssentialTags() {
     // GOOGLE TAG (Ads / conversions)
     (function loadGoogleTag() {
@@ -29,10 +39,12 @@
       const gtagScript = document.createElement('script');
       gtagScript.async = true;
       gtagScript.src = 'https://www.googletagmanager.com/gtag/js?id=AW-18022556253';
-      document.head.appendChild(gtagScript);
+      appendScript(gtagScript);
 
       window.dataLayer = window.dataLayer || [];
-      function gtag() { window.dataLayer.push(arguments); }
+      function gtag() {
+        window.dataLayer.push(arguments);
+      }
       window.gtag = gtag;
       gtag('js', new Date());
       gtag('config', 'AW-18022556253');
@@ -44,18 +56,20 @@
       w.__mousaTikTokLoaded = true;
 
       w.TiktokAnalyticsObject = t;
-      var ttq = w[t] = w[t] || [];
+      var ttq = (w[t] = w[t] || []);
       ttq.methods = [
         'page', 'track', 'identify', 'instances', 'debug',
         'on', 'off', 'once', 'ready', 'alias', 'group',
         'enableCookie', 'disableCookie', 'holdConsent',
         'revokeConsent', 'grantConsent'
       ];
+
       ttq.setAndDefer = function (obj, method) {
         obj[method] = function () {
           obj.push([method].concat(Array.prototype.slice.call(arguments, 0)));
         };
       };
+
       for (var i = 0; i < ttq.methods.length; i++) {
         ttq.setAndDefer(ttq, ttq.methods[i]);
       }
@@ -63,7 +77,7 @@
       ttq.load = function (pixelId, opts) {
         var url = 'https://analytics.tiktok.com/i18n/pixel/events.js';
         ttq._i = ttq._i || {};
-        ttq._i[pixelId] = [];
+        ttq._i[pixelId] = ttq._i[pixelId] || [];
         ttq._i[pixelId]._u = url;
         ttq._t = ttq._t || {};
         ttq._t[pixelId] = +new Date();
@@ -74,15 +88,24 @@
         script.type = 'text/javascript';
         script.async = true;
         script.src = url + '?sdkid=' + pixelId + '&lib=' + t;
+
         var firstScript = d.getElementsByTagName('script')[0];
-        firstScript.parentNode.insertBefore(script, firstScript);
+        if (firstScript && firstScript.parentNode) {
+          firstScript.parentNode.insertBefore(script, firstScript);
+        } else if (d.head) {
+          d.head.appendChild(script);
+        } else if (d.body) {
+          d.body.appendChild(script);
+        } else {
+          d.documentElement.appendChild(script);
+        }
       };
 
       ttq.load(id);
       ttq.page();
     })(window, document, 'ttq', 'D6ON94BC77U7PMTO33SG');
 
-    // LEAD / OTHER NON‑ESSENTIAL TAGS
+    // LEAD / OTHER NON-ESSENTIAL TAGS
     (function loadLeadTag() {
       if (window.__mousaLeadTagLoaded) return;
       window.__mousaLeadTagLoaded = true;
@@ -93,10 +116,10 @@
       s.id = 'vtag-ai-js';
       s.dataset.pid = '13svyJ0aec5qHxzuY';
       s.dataset.version = '062024';
-      document.head.appendChild(s);
+      appendScript(s);
     })();
 
-    // NOTE: Tally is loaded when the user reaches the form; keep as‑is.
+    // NOTE: Tally is loaded when the user reaches the form; keep as-is.
   }
 
   function hideBanner() {
@@ -111,14 +134,13 @@
 
   function applyConsent() {
     const consent = getConsent();
+
     if (consent && consent.accepted === true) {
       loadNonEssentialTags();
       hideBanner();
     } else if (consent && consent.accepted === false) {
       hideBanner();
-      // Non‑essential tags stay disabled.
     } else {
-      // No choice yet
       showBanner();
     }
   }
@@ -154,10 +176,13 @@
     init();
   }
 
-  // Optional: expose a small API so you can link \"Change cookie settings\" in the footer
   window.mousaCookieConsent = {
     reset: function () {
-      localStorage.removeItem(CONSENT_KEY);
+      try {
+        localStorage.removeItem(CONSENT_KEY);
+      } catch (e) {
+        // localStorage might be blocked; fail quietly
+      }
       showBanner();
     },
     get: getConsent
